96b20df99dd92d769f5cf7dd9cf7b0b6aa5914bc
mag-da-lenka <magdalenka.ag@gmail.com>
2022-11-12 13:50:08 +0100

    run prettier fixed

--- packages/client/src/containers/LandingPage/ProductsList.js
+++ packages/client/src/containers/LandingPage/ProductsList.js
@@ -6,7 +6,9 @@ export const ProductsList = ({ products }) => {
     <ul className="product-list">
       {products.map((product) => (
         <li key={product.id}>
-          <div>Spice {product.id}: &nbsp;{product.name}</div>
+          <div>
+            Spice {product.id}: &nbsp;{product.name}
+          </div>
         </li>
       ))}
     </ul>

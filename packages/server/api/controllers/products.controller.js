217248aa7a1d2adfaa77923da96786d7a8b0786e
shravanipagilla <nampally04@gmail.com>
2022-11-18 00:26:21 +0100

    swagger homework

--- packages/server/api/controllers/products.controller.js
+++ packages/server/api/controllers/products.controller.js
@@ -4,6 +4,18 @@ const getProducts = async () => {
   return knex('products').select('*');
 };
 
+const createProduct = async (body) => {
+  await knex('products').insert({
+    name: body.name,
+    description: body.description,
+  });
+
+  return {
+    successful: true,
+  };
+};
+
 module.exports = {
   getProducts,
+  createProduct,
 };
--- packages/server/api/routes/products.router.js
+++ packages/server/api/routes/products.router.js
@@ -6,6 +6,22 @@ const router = express.Router({ mergeParams: true });
 const productsController = require('../controllers/products.controller');
 
 // add SWAGGER documentation
+/**
+ * @swagger
+ * /products:
+ *  get:
+ *    tags:
+ *    - products
+ *    summary: Get all products
+ *    description:
+ *      Will return all products.
+ *    produces: application/json
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
 router.get('/', (req, res, next) => {
   productsController
     .getProducts()
@@ -13,4 +29,45 @@ router.get('/', (req, res, next) => {
     .catch(next);
 });
 
+/**
+ * @swagger
+ * /products:
+ *  post:
+ *    tags:
+ *    - products
+ *    summary: Create a product
+ *    description:
+ *      Will create a product.
+ *    produces: application/json
+ *    parameters:
+ *      - in: body
+ *        name: product
+ *        description: The product to create.
+ *        schema:
+ *          type: object
+ *          required:
+ *            - name:
+ *            - description:
+ *          properties:
+ *            name:
+ *              type: string
+ *            description:
+ *              type: string
+ *    responses:
+ *      201:
+ *        description: product created
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.post('/', (req, res) => {
+  productsController
+    .createProduct(req.body)
+    .then((result) => res.json(result))
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+
+      res.status(400).send('Bad request').end();
+    });
+});
 module.exports = router;

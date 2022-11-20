fc3121b93c3c62e3201b7a38205703725b726598
shravanipagilla <nampally04@gmail.com>
2022-11-20 09:14:55 +0100

    modified  navigation component

--- packages/client/src/components/Navigation/Navigation.jsx
+++ packages/client/src/components/Navigation/Navigation.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+// @ts-nocheck
+import React, { useRef } from 'react';
 import './Navigation.css';
-import { FaCartPlus, FaLock, FaCamera } from 'react-icons/fa';
+import { FaCartPlus, FaLock, FaCamera, FaBars, FaTimes } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 
 export const Navigation = ({ SignIn, Favourites, Cart, type, ...props }) => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle('responsive_nav');
+  };
+
   return (
     <nav className="example">
       <div className="navheader">
@@ -21,18 +28,24 @@ export const Navigation = ({ SignIn, Favourites, Cart, type, ...props }) => {
           <FaCartPlus />
           Cart
         </h4>
+        <button type="button" onClick={showNavbar}>
+          <FaBars />
+        </button>
       </div>
-      <div className="navbar">
+
+      <div className="navbar" ref={navRef}>
         <a href="/spices">Spices</a>
         <a href="/about">AboutUs</a>
         <a href="/contact">Contact</a>
+        <button type="button" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+
         <div className="search_input_box">
           <input
             className="search_input"
             type="text"
             placeholder="search for  product..."
-            // value={searchProduct}
-            // onChange={(e) => setSearchProduct(e.target.value)}
           />
         </div>
       </div>
@@ -41,25 +54,10 @@ export const Navigation = ({ SignIn, Favourites, Cart, type, ...props }) => {
 };
 
 Navigation.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
   SignIn: PropTypes.string,
-  /**
-   * What background color to use
-   */
   Favourites: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
   Cart: PropTypes.string,
-  /**
-   * Button contents
-   */
   type: PropTypes.string,
-  /**
-   * Optional click handler
-   */
 };
 Navigation.defaultProps = {
   SignIn: '',
